d91309dfb34e0606c34acd1b66371381eefe625f
computersarecool <willy@willynolan.com>
2018-03-06 20:37:48 -0800

    Removes duplicate files

--- container_output_calibrator/base_trigger_sequence_and_capture/text_chopexec1.py
+++
@@ -1,6 +0,0 @@
-def onValueChange(channel, sampleIndex, val, prev):
-	op.Decoder.op('tex3d_captures1').par.replaceindex = val
-	op.Decoder.op('tex3d_captures1').par.resetsinglepulse.pulse()
-	
-	
-	
