21b42b8df5e69360a72da928bfb070672c37443a
michaelwalczyk <michael.walczyk@obscuradigital.com>
2018-08-09 17:54:50 -0700

    Adds gitignore and stubs for grid smoothing algo.

--- touchdesigner/container_output_calibrator/base_trigger_sequence_and_capture/text_class_sequencer.py
+++ touchdesigner/container_output_calibrator/base_trigger_sequence_and_capture/text_class_sequencer.py
@@ -4,7 +4,10 @@ import numpy as np
 class Sequencer:
 
 	def __init__(self):
-		pass
+		self.load_path = '{}/../assets/temp/{}'.format(project.folder, 'white.jpg')
+		self.save_path = '{}/../assets/temp/{}'.format(project.folder, 'bounding.jpg')
+		self.mask_color = (255, 255, 255)
+		self.dilation_iterations = 1
 
 	def Run(self):
 		# Switch to all white
@@ -16,22 +19,31 @@ class Sequencer:
 		op.ProjectorOutputs.op('switch_projection_map').par.index = 0
 		op('lfo_start').par.resetpulse.pulse()
 
-	def Calculate_bounds(self):
-		load_path = '{}/../assets/temp/{}'.format(project.folder, 'white.jpg')
-		save_path = '{}/../assets/temp/{}'.format(project.folder, 'bounding.jpg')
-		op('switch_device').save(load_path)
+	def Calculate_bounds(self, draw_contour=True, blur=False):
+		op('switch_device').save(self.load_path)
 
 		# Read image
-		image = cv2.imread(load_path)
+		image = cv2.imread(self.load_path)
 		image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 		ret, thresh = cv2.threshold(image_gray, 127, 255, 0)
 		image_result, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 		# Find the contour with the largest area
-		largest_contour = max(contours, key = cv2.contourArea)
+		largest_contour = max(contours, key=cv2.contourArea)
 		x, y, w, h = cv2.boundingRect(largest_contour)
 
-		# Draw the contour
+		# Draw the contour or bounding rectangle
 		blank_image = np.zeros((parent.Calibrator.par.Projectorresolution2, parent.Calibrator.par.Projectorresolution1, 3), np.uint8)
-		cv2.rectangle(blank_image, (x, y), (x + w, y + h), (255, 255, 255), cv2.FILLED)
-		cv2.imwrite(save_path, blank_image)
+		if draw_contour:
+			cv2.drawContours(blank_image, [largest_contour], 0, self.mask_color, cv2.FILLED)
+		else:
+			cv2.rectangle(blank_image, (x, y), (x + w, y + h), self.mask_color, cv2.FILLED)
+
+		kernel = np.ones((5, 5), np.uint8)
+		blank_image = cv2.dilate(blank_image, kernel, iterations=self.dilation_iterations)
+		
+		# Optionally blur the mask
+		if blur:
+			blank_image = cv2.GaussianBlur(blank_image, (5, 5), 0)
+
+		cv2.imwrite(self.save_path, blank_image)
